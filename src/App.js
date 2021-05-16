dacd5188b4025aad4f50e0e7a62e44a8f14ac05f
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-16 19:20:48 -0400

    convered it to a class component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React, { Component } from 'react';
 import './App.css';
 
-const App = () => {
-  return <div className="App">Hello World</div>;
-};
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
