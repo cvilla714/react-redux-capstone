2a9856663fc32dbde86600bab4f74aba4f88be20
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-17 18:55:35 -0400

    created the User component to pull out the information from the API form  a single user, gettign name, avatar, location,bio, repos and more

---
+++ src/components/users/User.js
@@ -0,0 +1,35 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+
+class User extends Component {
+  componentDidMount() {
+    this.props.getUser(this.props.match.params.login);
+  }
+
+  render() {
+    const {
+      name,
+      avatar_url,
+      location,
+      bio,
+      blog,
+      login,
+      html_url,
+      followers,
+      following,
+      public_repos,
+      public_gists,
+      hireable,
+    } = this.props.user;
+
+    const { loading } = this.props;
+
+    return <div>{name}</div>;
+  }
+}
+
+export default User;
