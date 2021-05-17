6bb9de97b1a9f26498c72634a883f374f8f28d42
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-17 00:35:41 -0400

    converted to cammel case variables

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -8,20 +8,20 @@ class Users extends Component {
       {
         id: '1',
         login: 'mojombo',
-        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
+        avatarUrl: 'https://avatars.githubusercontent.com/u/1?v=4',
+        htmlUrl: 'https://github.com/mojombo',
       },
       {
         id: '2',
         login: 'defunkt',
-        avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt',
+        avatarUrl: 'https://avatars.githubusercontent.com/u/2?v=4',
+        htmlUrl: 'https://github.com/defunkt',
       },
       {
         id: '3',
         login: 'pjhyett',
-        avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/pjhyett',
+        avatarUrl: 'https://avatars.githubusercontent.com/u/3?v=4',
+        htmlUrl: 'https://github.com/pjhyett',
       },
     ],
   };
