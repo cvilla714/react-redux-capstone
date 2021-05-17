6552ba90265bf682c915cbc954a99bb193a3900e
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-17 01:30:00 -0400

    changed back from cammelCaseto snake format to match the information from the API

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,17 +1,19 @@
+/* eslint-disable camelcase */
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const UserItem = ({ user: { login, avatarUrl, htmlUrl } }) => (
+const UserItem = ({ user: { login, avatar_url, html_url } }) => (
   <div className="card text-center">
     <img
-      src={avatarUrl}
+      src={avatar_url}
       alt=""
       className="round-img"
       style={{ width: '60px' }}
     />
     <h3>{login}</h3>
     <div>
-      <a href={htmlUrl} className="btn btn-dark btn-sm-my-1">
+      <a href={html_url} className="btn btn-dark btn-sm-my-1">
         More Info
       </a>
     </div>
@@ -19,7 +21,7 @@ const UserItem = ({ user: { login, avatarUrl, htmlUrl } }) => (
 );
 
 UserItem.propTypes = {
-  user: PropTypes.arrayOf(Object).isRequired,
+  user: PropTypes.object.isRequired,
 };
 
 export default UserItem;
