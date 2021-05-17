d7ad0e69d3b3b1203924b3ebc2774f12e3d09787
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-17 02:20:11 -0400

    passing the user and loading as props and switch to a functional stateless component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,25 +1,34 @@
+/* eslint-disable react/forbid-prop-types */
 /* eslint-disable react/prop-types */
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+
+import React from 'react';
+import PropTypes from 'prop-types';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
 
-class Users extends Component {
-  render() {
-    const { users } = this.props;
-    const userStyle = {
-      display: 'grid',
-      gridTemplateColumns: 'repeat(3,1fr)',
-      gridGap: '1rem',
-    };
-
-    return (
-      <div style={userStyle}>
-        {users.map((user) => (
-          <UserItem key={user.id} user={user} />
-        ))}
-      </div>
-    );
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
   }
-}
+
+  const userStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3,1fr)',
+    gridGap: '1rem',
+  };
+
+  return (
+    <div style={userStyle}>
+      {users.map((user) => (
+        <UserItem key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 
 export default Users;
