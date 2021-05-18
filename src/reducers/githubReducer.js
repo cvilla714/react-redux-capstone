b4a6f5e69044269b173fbd468a29236db121c000
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-18 15:15:28 -0400

    created the cases for searchUsers and setLoading

--- src/reducers/githubReducer.js
+++ src/reducers/githubReducer.js
@@ -1,14 +1,26 @@
-import {
-  SEARCH_USERS,
-  SET_LOADING,
-  CLEAR_USERS,
-  GET_USER,
-  GET_REPOS,
-} from '../actions/index';
+import { SEARCH_USERS, SET_LOADING } from '../actions/types';
 
 const initialState = {
   users: [],
   user: {},
   repos: [],
   loading: false,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SEARCH_USERS:
+      return {
+        ...state,
+        users: action.payload,
+        laoding: false,
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    default:
+      return state;
+  }
 };
