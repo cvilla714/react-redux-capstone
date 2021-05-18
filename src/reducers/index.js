552272a14c44df363590773530204a2a0f30a3e0
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-18 15:15:51 -0400

    added the githubReducer to the combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,6 @@
 import { combineReducers } from 'redux';
+import githubReducer from './githubReducer';
 
-export default combineReducers;
+export default combineReducers({
+  github: githubReducer,
+});
