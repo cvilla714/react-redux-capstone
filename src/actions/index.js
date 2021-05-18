81a24fae9b75c9897b14522c71efb928111245be
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-18 15:14:12 -0400

    created the searchUsers and setLoading functions to send to the API the request from the input

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,33 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/prefer-default-export */
 import axios from 'axios';
-import {
-  SEARCH_USERS,
-  SET_LOADING,
-  CLEAR_USERS,
-  GET_REPOS,
-  GET_USER,
-} from './types';
+import { SEARCH_USERS, SET_LOADING } from './types';
+
+//  Set Loading
+export const setLoading = () => ({
+  type: SET_LOADING,
+});
 
 //  Search Users
+export const searchUsers = (text) => async (dispatch) => {
+  //   setLoading(true);
+  setLoading();
+
+  const res = await axios.get(`https://api.github.com/search/users?q=${text}`, {
+    headers: {
+      Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+    },
+  });
+  dispatch({
+    type: SEARCH_USERS,
+    payload: res.data.items,
+  });
+  //   setUsers(res.data.items);
+  //   setLoading(false);
+};
 
 //  Get user
 
 //  Get Repos
 
 //  Clear Users
-
-//  Set Loading
