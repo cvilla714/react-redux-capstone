dc0a5a5bec14c310be6f7689bf2019a91439d5a6
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-18 12:49:16 -0400

    imported the action types and created the boiler plate for all actions, searcUsers,getUsers,getRepos,getUsers,clearUsers

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import {
+  SEARCH_USERS,
+  SET_LOADING,
+  CLEAR_USERS,
+  GET_REPOS,
+  GET_USER,
+} from './types';
+
+//  Search Users
+
+//  Get user
+
+//  Get Repos
+
+//  Clear Users
+
+//  Set Loading
