3e951c3f2bac69824d484633d05bb18299671d59
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-19 17:26:48 -0400

    created the tes for Repos component

---
+++ src/test/Repos.test.js
@@ -0,0 +1,16 @@
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { create } from 'react-test-renderer';
+import { createStore, applyMiddleware } from 'redux';
+import combineReducers from '../reducers';
+import Repos from '../components/repos/Repos';
+
+test('Repos renders correctly', () => {
+  const store = createStore(combineReducers, applyMiddleware(thunk));
+  const tree = create(
+    <Provider store={store}>
+      <Repos />
+    </Provider>,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
