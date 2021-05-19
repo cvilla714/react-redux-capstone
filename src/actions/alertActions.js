419441d91b7bee24a148c6d018a08a9775db7b9c
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-19 02:11:31 -0400

    added the setTimout function

--- src/actions/alertActions.js
+++ src/actions/alertActions.js
@@ -1,8 +1,15 @@
-import { SET_ALERT } from './types';
+import { REMOVE_ALERT, SET_ALERT } from './types';
 
 const setAlert = (msg, type) => ({
   type: SET_ALERT,
   payload: { msg, type },
 });
+
+setTimeout(
+  () => ({
+    type: REMOVE_ALERT,
+  }),
+  3000,
+);
 
 export default setAlert;
