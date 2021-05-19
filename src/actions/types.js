2ccaa758019957bda1776678d94656b12c11a15d
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-19 02:35:24 -0400

    added the logsError action

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,5 +3,6 @@ export const GET_USER = 'GET_USER';
 export const CLEAR_USERS = 'CLEAR_USERS';
 export const GET_REPOS = 'GET_REPOS';
 export const SET_LOADING = 'SET_LOADING';
+export const LOGS_ERROR = 'LOGS_ERROR';
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
