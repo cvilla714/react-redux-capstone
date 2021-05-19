4fc00988da3060dda84bbe47bac2f7686ae00e7f
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-19 02:11:12 -0400

    added the Home and NotFound pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import Home from './components/pages/Home';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import User from './components/users/User';
 import store from './store';
 import './App.css';
@@ -19,19 +18,10 @@ const App = () => (
         <div className="container">
           <Alert />
           <Switch>
-            <Route
-              exact
-              path="/"
-              // eslint-disable-next-line no-unused-vars
-              render={(props) => (
-                <>
-                  <Search />
-                  <Users />
-                </>
-              )}
-            />
+            <Route exact path="/" component={Home} />
             <Route exact path="/about" component={About} />
             <Route exact path="/user/:login" component={User} />
+            <Route component={NotFound} />
           </Switch>
         </div>
       </div>
