5a0e9d7b58fbf5bc5f9077601d07958b527c3182
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-19 01:44:21 -0400

    added the alertReducer to the combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import githubReducer from './githubReducer';
+import alertReducer from './alertReducer';
 
 export default combineReducers({
   github: githubReducer,
+  alert: alertReducer,
 });
