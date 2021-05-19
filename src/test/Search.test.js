52690992549a3fca20bd5cba4febdad5ad441d52
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-19 17:27:04 -0400

    created the tes for Search component

---
+++ src/test/Search.test.js
@@ -0,0 +1,16 @@
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { create } from 'react-test-renderer';
+import { createStore, applyMiddleware } from 'redux';
+import combineReducers from '../reducers';
+import Search from '../components/users/Search';
+
+test('Search renders correctly', () => {
+  const store = createStore(combineReducers, applyMiddleware(thunk));
+  const tree = create(
+    <Provider store={store}>
+      <Search />
+    </Provider>,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
