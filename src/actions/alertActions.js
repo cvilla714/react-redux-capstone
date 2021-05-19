c7700545b4b3e41ae961d78e7a96f3485ec6572c
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-19 11:53:38 -0400

    added the remove alert action

--- src/actions/alertActions.js
+++ src/actions/alertActions.js
@@ -1,8 +1,12 @@
-import { SET_ALERT } from './types';
+import { REMOVE_ALERT, SET_ALERT } from './types';
 
 const setAlert = (payload) => ({
   type: SET_ALERT,
   payload,
+});
+
+export const removeAlert = () => ({
+  type: REMOVE_ALERT,
 });
 
 export default setAlert;
