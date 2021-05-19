e618d2e8ea45f8bf2e171960cfdeeaecb0c7dfd8
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-18 20:05:00 -0400

    fixed the propType for repos

--- src/components/repos/Repos.js
+++ src/components/repos/Repos.js
@@ -1,15 +1,12 @@
-/* eslint-disable react/forbid-prop-types */
-/* eslint-disable implicit-arrow-linebreak */
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import RepoItem from './RepoItem';
 
-const Repos = ({ repos }) =>
-  repos.map((repo) => <RepoItem repo={repo} key={repo.id} />);
+const Repos = ({ repos }) => repos.map((repo) => <RepoItem repo={repo} key={repo.id} />);
 
 Repos.propTypes = {
-  repos: PropTypes.array.isRequired,
+  repos: PropTypes.arrayOf(Array).isRequired,
 };
 
 const mapStateToProps = (state) => ({
