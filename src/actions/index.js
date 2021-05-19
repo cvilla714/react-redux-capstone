c772fd92a374f5e702d90ec05a587bf4c59d57d0
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-19 02:35:01 -0400

    added try and catch to all request to the API

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,7 @@ import {
   GET_USER,
   SEARCH_USERS,
   SET_LOADING,
+  LOGS_ERROR,
 } from './types';
 
 export const setLoading = () => ({
@@ -12,49 +13,73 @@ export const setLoading = () => ({
 });
 
 export const searchUsers = (text) => async (dispatch) => {
-  setLoading();
+  try {
+    setLoading();
 
-  const res = await axios.get(`https://api.github.com/search/users?q=${text}`, {
-    headers: {
-      Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-    },
-  });
-  dispatch({
-    type: SEARCH_USERS,
-    payload: res.data.items,
-  });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}`,
+      {
+        headers: {
+          Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        },
+      },
+    );
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.respose.statusText,
+    });
+  }
 };
 
 export const getUser = (username) => async (dispatch) => {
-  setLoading();
+  try {
+    setLoading();
 
-  const res = await axios.get(`https://api.github.com/users/${username}`, {
-    headers: {
-      Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-    },
-  });
-  dispatch({
-    type: GET_USER,
-    payload: res.data,
-  });
-};
-
-export const getUserRepos = (username) => async (dispatch) => {
-  setLoading();
-
-  const res = await axios.get(
-    `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc`,
-    {
+    const res = await axios.get(`https://api.github.com/users/${username}`, {
       headers: {
         Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
       },
-    },
-  );
+    });
+    dispatch({
+      type: GET_USER,
+      payload: res.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.respose.statusText,
+    });
+  }
+};
 
-  dispatch({
-    type: GET_REPOS,
-    payload: res.data,
-  });
+export const getUserRepos = (username) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc`,
+      {
+        headers: {
+          Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        },
+      },
+    );
+
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.respose.statusText,
+    });
+  }
 };
 
 export const clearUsers = () => ({
