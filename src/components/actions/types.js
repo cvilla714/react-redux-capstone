2e9e24e8f6719c13abb423d97e24889b554781c6
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-20 19:07:00 -0400

    adding the filter by name action

--- src/components/actions/types.js
+++ src/components/actions/types.js
@@ -6,3 +6,4 @@ export const SET_LOADING = 'SET_LOADING';
 export const LOGS_ERROR = 'LOGS_ERROR';
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
+export const FILTER_BY_NAME = 'FILTER_BY_NAME';
