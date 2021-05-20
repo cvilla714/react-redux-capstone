238838da62a2bc10a9cbf91cdc1587e9d63b3459
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-20 19:09:28 -0400

    added the filterReducer to the combine Reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import githubReducer from './githubReducer';
 import alertReducer from './alertReducer';
+import filterReducer from './filterReducer';
 
 export default combineReducers({
   github: githubReducer,
   alert: alertReducer,
+  filter: filterReducer,
 });
