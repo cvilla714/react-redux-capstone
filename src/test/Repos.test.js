971323edf91858dd1df0992bdf3e6516247eca36
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-20 13:08:26 -0400

    fixed path for Repos component

--- src/test/Repos.test.js
+++ src/test/Repos.test.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { create } from 'react-test-renderer';
 import { createStore, applyMiddleware } from 'redux';
 import combineReducers from '../reducers';
-import Repos from '../components/repos/Repos';
+import Repos from '../containers/repos/Repos';
 
 test('Repos renders correctly', () => {
   const store = createStore(combineReducers, applyMiddleware(thunk));
