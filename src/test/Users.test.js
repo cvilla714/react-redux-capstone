18b8b3ba0bb46d7515d2e36c3f7bf311c820530e
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-20 13:09:14 -0400

    fixed the path for the Users component

--- src/test/Users.test.js
+++ src/test/Users.test.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { create } from 'react-test-renderer';
 import { createStore, applyMiddleware } from 'redux';
 import combineReducers from '../reducers';
-import Users from '../components/users/Users';
+import Users from '../containers/users/Users';
 
 test('Users renders correctly', () => {
   const store = createStore(combineReducers, applyMiddleware(thunk));
