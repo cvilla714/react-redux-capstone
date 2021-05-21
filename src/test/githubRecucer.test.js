c2d92016a043a94bb34c824d0a05bed6d877d510
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-20 23:09:05 -0400

    added test for githubReducer

--- src/test/githubRecucer.test.js
+++ src/test/githubRecucer.test.js
@@ -1,1 +1,12 @@
 import githubReducer from '../reducers/githubReducer';
+
+describe('githubReducer', () => {
+  it('should return the initial state', () => {
+    expect(githubReducer(undefined, {})).toEqual({
+      users: [],
+      user: {},
+      repos: [],
+      loading: false,
+    });
+  });
+});
