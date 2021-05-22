b1ecde222386aa52b3ad78900ed04e8ea9da7954
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-21 21:11:24 -0400

    Changed the div for a react fragment

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const About = () => (
-  <div>
+  <>
     <h1>About This App</h1>
     <p>App to search GitHub Users</p>
     <p>Version: 1.0.0</p>
-  </div>
+  </>
 );
 
 export default About;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,10 +3,10 @@ import Search from '../../containers/users/Search';
 import Users from '../../containers/users/Users';
 
 const Home = () => (
-  <div>
+  <>
     <Search />
     <Users />
-  </div>
+  </>
 );
 
 export default Home;
