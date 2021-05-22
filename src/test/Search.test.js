07710ffa7dce379b0c7946b6e17349a62b352b0a
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-21 21:19:51 -0400

    Fixed path for combinedReducers

--- src/test/Search.test.js
+++ src/test/Search.test.js
@@ -2,7 +2,7 @@ import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { create } from 'react-test-renderer';
 import { createStore, applyMiddleware } from 'redux';
-import combineReducers from '../reducers';
+import combineReducers from '../redux/reducers/index';
 import Search from '../containers/users/Search';
 
 test('Search renders correctly', () => {
