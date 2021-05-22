e0413887cb0d0f1e7c24fd94d6c9bb372a7a5d22
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-21 21:11:59 -0400

    Added default props and correct proptype for repo

--- src/components/repos/RepoItem.js
+++ src/components/repos/RepoItem.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const RepoItem = ({ repo }) => (
+const RepoItem = ({ repo: { html_url: htmlUrl, name } }) => (
   <div className="card">
     <h3>
-      <a href={repo.html_url}>{repo.name}</a>
+      <a href={htmlUrl}>{name}</a>
     </h3>
   </div>
 );
 
+RepoItem.defaultProps = {
+  repo: [],
+  name: '',
+};
+
 RepoItem.propTypes = {
-  repo: PropTypes.arrayOf(Array).isRequired,
-  html_url: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
+  repo: PropTypes.objectOf(Object),
+  name: PropTypes.string,
 };
 
 export default RepoItem;
