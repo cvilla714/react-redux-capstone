5cc85d2bd7eb09bade5a2b57e37c92f82ddeddd1
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-21 21:13:11 -0400

    Added correct path for filter action

--- src/containers/users/FilterByName.js
+++ src/containers/users/FilterByName.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { connect } from 'react-redux';
-import filter from '../../components/actions/filterAction';
+import filter from '../../redux/actions/filterAction';
 
 const FilterByName = ({ filter, className }) => {
   const [filtering, setFiltering] = useState(false);
